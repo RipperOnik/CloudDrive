8bc2fe49887d57e8a2d2cba480fc53cc2aec07f0
RipperOnik <kingstrong2@mail.ru>
2023-03-09 20:40:41 +0300

    fixed the details header

--- src/components/google-drive/Details.js
+++ src/components/google-drive/Details.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { divideFileName } from './File'
 import "../../styles/detail.css"
 
 
@@ -16,23 +15,17 @@ export default function Details({ element, setShowDetails, showDetails }) {
   }, [showDetails])
 
   if (element) {
-    const [fileName, fileExtension] = divideFileName(element.name)
     const isFile = element.url
     const size = convertSize(element.size)
     const createdAt = defineDate(element.createdAt)
     return (
       <div className='details' style={{ width: width, padding: showDetails ? '15px' : '0' }}>
         <div ref={ref}>
-          <div className='details-header'>
-            <a href={element.url} target="_blank" className='details-file text-truncate d-flex align-items-center' style={{ gap: "8px", width: "200px" }}>
-              <img src={isFile ? `./images/${element.type}.svg` : "./images/folder.svg"} alt="file" style={{ width: "35px" }} onError={(e) => e.target.src = "./images/file.svg"} />
-              <div className='d-flex flex-grow-1 text-truncate'>
-                <div className='text-truncate'>{isFile ? fileName : element.name}</div>
-                {isFile && <span>{fileExtension}</span>}
-              </div>
-            </a>
-            {showDetails && <button type='button' className='btn-close' style={{ width: "2px" }} onClick={() => setShowDetails(false)} />}
-          </div>
+          <a href={element.url} target="_blank" className='details-file d-flex' style={{ gap: "8px" }}>
+            <img src={isFile ? `./images/${element.type}.svg` : "./images/folder.svg"} alt="file" style={{ width: "35px" }} onError={(e) => e.target.src = "./images/file.svg"} />
+            {element.name}
+          </a>
+          {showDetails && <button type='button' className='btn-close' style={{ width: "2px", position: "absolute", right: "7px", top: "7px" }} onClick={() => setShowDetails(false)} />}
           <div className='details-body'>
             <Detail name="Size" value={size} />
             <Detail name="Type" value={capitalize(isFile ? element.type : "folder")} />
